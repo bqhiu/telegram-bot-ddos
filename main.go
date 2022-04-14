77e70850a782244cde5bbb9d21c00a57bcff2ef6
pussybaka <87627629+pussybaka@users.noreply.github.com>
2022-04-14 16:03:35 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		panic(err)
+	}
+	updateConfigs := tgbotapi.NewUpdate(0)
+	updateConfigs.Timeout = 0
+	updates := bot.GetUpdatesChan(updateConfigs)
+
+	/**************************************************************/
+	AddCommand("help", help)
+	AddCommand("cat", cat)
+	/**************************************************************/
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		cmdAction, _ := ParseCommand(update.Message.Text)
+		action, ok := actions[cmdAction].(func(*tgbotapi.BotAPI, tgbotapi.Update))
+		if !ok {
+			continue
+		}
+		go action(bot, update)
+	}
+}
+
